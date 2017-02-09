bef908a759ac3634dde735282605b39d5b0ee179
hujun <azhujun_githun@163.com>
2017-02-09 20:34:18 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo json_encode([
+    'code'=>200,
+    'list'=>[
+        [
+            'title'=>'xxx',
+        ],
+        [
+            'title'=>'xxx',
+        ],
+        [
+            'title'=>'xxx',
+        ]
+    ]
+]);
+
+?>
